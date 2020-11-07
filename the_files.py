57cef619e733663c2f2c9df648e6ff798b471f66
Angelos1 <prastitisangelos@gmail.com>
2020-11-07 08:30:30 +0200

    OOP applied

--- the_files.py
+++ the_files.py
@@ -1,11 +1,18 @@
 import utilities as u
 import sys
 
-class ReadDictionary:
-    def __init__(self,file_path):
-        self.dictionary = u.read_file(file_path)
+class TheFiles:
+    def __init__(self, opts):
+        for opt, arg in opts:
+            if opt == "--dictionary":
+                # dictionary = self.validate_dict()
+                self.dictionary = u.read_file(arg)
+                self.validate_dict()
 
+            else:
+                self.input = u.read_file(arg)[0]
 
+    # validates the limits for the dictionary
     def validate_dict(self):
         # print(self.dictionary)
         word_lengths = [len(word) for word in self.dictionary]
@@ -13,16 +20,15 @@ class ReadDictionary:
         unique_words = set(self.dictionary)
         min_length = min(word_lengths)
         max_length = max(word_lengths)
-
         # limits
         if len(unique_words) != len(self.dictionary):
             print("Dictionary off limits: Two or more words in the dictionary are the same")
             sys.exit()
-        if min_length < 2 or max_length >105:
+        if min_length < 2 or max_length > 105:
             print("Dictionary off limits: Each word in the dictionary has to be between 2 and 105 letters long")
             sys.exit()
         if sum(word_lengths) > 105:
             print("Dictionary off limits: The sum of lengths of all words in the dictionary exceeds 105")
             sys.exit()
-        return self.dictionary
+
 
--- utilities.py
+++ utilities.py
@@ -1,9 +1,14 @@
+import sys
 
 def read_file(file_path):
-    file = open(file_path, 'r')
-    # words = file.readlines()
-    words = file.read().splitlines()
-    file.close()
-    return words
+    try:
+        file = open(file_path, 'r')
+        # words = file.readlines()
+        words = file.read().splitlines()
+        file.close()
+        return words
+    except FileNotFoundError:
+        print('File not Found: "' + file_path +'"' )
+        sys.exit()
 
 # dictionary_list("Files/dictionary1")
