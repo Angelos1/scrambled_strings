58340621e04787f2b0b80972c23c7ae4bc0d92e5
Angelos1 <prastitisangelos@gmail.com>
2020-11-09 16:56:52 +0200

    Algorithm complete

--- scrambled_words.py
+++ scrambled_words.py
@@ -2,9 +2,9 @@ from dictionary_word import DictionaryWord
 
 
 class ScrambledWords:
-    def __init__(self, dictionary, text):
+    def __init__(self, dictionary, input_strings):
         self.dictionary = dictionary
-        self.text = text
+        self.input_strings = input_strings
 
     # def curr_word_in_text(self,char_index, curr_dict_word, dict_word):
     #     curr_dict_word.first_char = self.text[char_index]
@@ -14,7 +14,7 @@ class ScrambledWords:
     '''finds the next substring of the text in the input file, the one that 
     will be compared to the current dictionary_word
     '''
-    def __curr_word_in_text(self, char_index, curr_dict_word, dict_word):
+    def __curr_word_in_text(self, char_index, long_string, curr_dict_word, dict_word):
         # print(char_index)
         # print(char_index + len(dict_word) - 1)
         # print(ord(self.text[char_index])-97)
@@ -22,32 +22,34 @@ class ScrambledWords:
         # print(self.text[char_index])
         # print(self.text[char_index + len(dict_word) - 2])
 
-        curr_dict_word.first_char = self.text[char_index]
-        curr_dict_word.last_char = self.text[char_index + len(dict_word) - 1]
-        curr_dict_word.freq_arr_middle[ord(self.text[char_index])-97] -= 1
-        curr_dict_word.freq_arr_middle[ord(self.text[char_index + len(dict_word) - 2])-97] += 1
 
-    def algorithm(self, dictionary, input, text):
+        curr_dict_word.first_char = long_string[char_index]
+        curr_dict_word.last_char = long_string[char_index + len(dict_word) - 1]
+        curr_dict_word.freq_arr_middle[ord(long_string[char_index]) - 97] -= 1
+        curr_dict_word.freq_arr_middle[ord(long_string[char_index + len(dict_word) - 2]) - 97] += 1
+
+    # def algorithm(self, dictionary, input, text):
+    def get_scrambled_words_occurences(self):
+
         line_number = 1
-        for long_string in input:
+        for long_string in self.input_strings:
             chars_in_text = len(long_string)
             count = 0
-            for dict_word in dictionary: # going through all the words in the dictionary
+            for dict_word in self.dictionary: # going through all the words in the dictionary
                 curr_dict_word = DictionaryWord(dict_word)
                 curr_word_in_text = DictionaryWord(long_string[0:len(dict_word):]) # current area of the text that we compare to the dictionary word
                 for i in range(chars_in_text):
                     if (i + len(dict_word) <= chars_in_text):
-                        self.__curr_word_in_text(i, curr_word_in_text, dict)
+                        self.__curr_word_in_text(i, long_string, curr_word_in_text, dict_word)
                         if curr_dict_word.equals(curr_word_in_text):
                             count += 1
                             break;
-            print("Case #"+line_number+": "+count)
+            print("Case #" + str(line_number) + ": " + str(count))
             line_number+=1
 
 
 
 
-                    len_str = len(str)
 
 
 
